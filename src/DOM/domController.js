3fb7e4e36dc6ffffd21f25aafa3065e76dcc2a63
Sasoun Torossian <sasountorossian@gmail.com>
2020-10-17 17:32:39 +0100

    Styled task details section.

--- src/DOM/domController.js
+++ src/DOM/domController.js
@@ -156,6 +156,7 @@ const DomController = (() => {
             if(task.priority == 1) titleTask.style.color = "green"
             else if(task.priority == 2) titleTask.style.color = "orange"
             else if(task.priority == 3) titleTask.style.color = "red"
+            else titleTask.style.color = "green"
             containerTask.appendChild(titleTask)
             
             const editTask = document.createElement("div")
@@ -201,42 +202,156 @@ const DomController = (() => {
         const containerTaskDetails = document.createElement("div") 
         containerTaskDetails.classList.add("taskDetails")
 
+        // First Row
+        const titleDetailsContainer = document.createElement("div")
+        titleDetailsContainer.classList.add("detailsContainer")
+
+        const titleTaskDetailsLabel = document.createElement("label")
+        titleTaskDetailsLabel.classList.add("titleTaskDetailsLabel")
+        titleTaskDetailsLabel.classList.add("label")
+        titleTaskDetailsLabel.for = "titleTaskDetails"
+        titleTaskDetailsLabel.innerText = "Title"
+        titleDetailsContainer.appendChild(titleTaskDetailsLabel)
+
         const titleTaskDetails = document.createElement("div")
-        titleTaskDetails.classList.add("titleTaskDetail")
+        titleTaskDetails.classList.add("titleTaskDetails")
+        titleTaskDetails.classList.add("detail")
         titleTaskDetails.innerText = task.title
-        containerTaskDetails.appendChild(titleTaskDetails)
+        titleDetailsContainer.appendChild(titleTaskDetails)
+
+        const priorityDetailsContainer = document.createElement("div")
+        priorityDetailsContainer.classList.add("detailsContainer")
+
+        const priorityTaskDetailsLabel = document.createElement("label")
+        priorityTaskDetailsLabel.classList.add("priorityTaskDetailsLabel")
+        priorityTaskDetailsLabel.classList.add("label")
+        priorityTaskDetailsLabel.for = "priorityTaskDetails"
+        priorityTaskDetailsLabel.innerText = "Priority"
+        priorityDetailsContainer.appendChild(priorityTaskDetailsLabel)
+
+        const priorityTaskDetails = document.createElement("div")
+        priorityTaskDetails.classList.add("priorityTaskDetails")
+        priorityTaskDetails.classList.add("detail")
+        if(task.priority == 1) priorityTaskDetails.innerText = "Low"
+        else if(task.priority == 2) priorityTaskDetails.innerText = "Medium"
+        else if(task.priority == 3) priorityTaskDetails.innerText = "High"
+        else priorityTaskDetails.innerText = "Low"
+        priorityDetailsContainer.appendChild(priorityTaskDetails)
+
+        const firstRow = document.createElement("div")
+        firstRow.classList.add("firstRow")
+        firstRow.classList.add("row")
+        firstRow.appendChild(titleDetailsContainer)
+        firstRow.appendChild(priorityDetailsContainer)
+        containerTaskDetails.appendChild(firstRow)
+
+        // Second Row
+        const descDetailsContainer = document.createElement("div")
+        descDetailsContainer.classList.add("detailsContainer")
+
+        const descTaskDetailsLabel = document.createElement("label")
+        descTaskDetailsLabel.classList.add("descTaskDetailsLabel")
+        descTaskDetailsLabel.classList.add("label")
+        descTaskDetailsLabel.for = "descTaskDetail"
+        descTaskDetailsLabel.innerText = "Description"
+        descDetailsContainer.appendChild(descTaskDetailsLabel)
 
         const descTaskDetails = document.createElement("div")
         descTaskDetails.classList.add("descTaskDetail")
+        descTaskDetails.classList.add("detail")
         descTaskDetails.innerText = task.desc
-        containerTaskDetails.appendChild(descTaskDetails)
+        descDetailsContainer.appendChild(descTaskDetails)
+
+        const secondRow = document.createElement("div")
+        secondRow.classList.add("secondRow")
+        secondRow.classList.add("row")
+        secondRow.appendChild(descDetailsContainer)
+        containerTaskDetails.appendChild(secondRow)
+
+        // Third Row
+        const dateDetailsContainer = document.createElement("div")
+        dateDetailsContainer.classList.add("detailsContainer")
+
+        const dateTaskDetailsLabel = document.createElement("label")
+        dateTaskDetailsLabel.classList.add("dateTaskDetailsLabel")
+        dateTaskDetailsLabel.classList.add("label")
+        dateTaskDetailsLabel.for = "dateTaskDetail"
+        dateTaskDetailsLabel.innerText = "Due Date"
+        dateDetailsContainer.appendChild(dateTaskDetailsLabel)
 
         const dateTaskDetails = document.createElement("div")
         dateTaskDetails.classList.add("dateTaskDetail")
+        dateTaskDetails.classList.add("detail")
         dateTaskDetails.innerText = Time.getDate(task.date)
-        containerTaskDetails.appendChild(dateTaskDetails)
+        dateDetailsContainer.appendChild(dateTaskDetails)
+
+        /////
+        const timeDetailsContainer = document.createElement("div")
+        timeDetailsContainer.classList.add("detailsContainer")
+
+        const timeTaskDetailsLabel = document.createElement("label")
+        timeTaskDetailsLabel.classList.add("timeTaskDetailsLabel")
+        timeTaskDetailsLabel.classList.add("label")
+        timeTaskDetailsLabel.for = "timeTaskDetail"
+        timeTaskDetailsLabel.innerText = "Due Time"
+        timeDetailsContainer.appendChild(timeTaskDetailsLabel)
 
         const timeTaskDetails = document.createElement("div")
         timeTaskDetails.classList.add("timeTaskDetail")
+        timeTaskDetails.classList.add("detail")
         timeTaskDetails.innerText = Time.getTime(task.date)
-        containerTaskDetails.appendChild(timeTaskDetails)
+        timeDetailsContainer.appendChild(timeTaskDetails)
+
+        /////
+        const timeLeftDetailsContainer = document.createElement("div")
+        timeLeftDetailsContainer.classList.add("detailsContainer")
+
+        const timeLeftTaskDetailsLabel = document.createElement("label")
+        timeLeftTaskDetailsLabel.classList.add("timeLeftTaskDetailsLabel")
+        timeLeftTaskDetailsLabel.classList.add("label")
+        timeLeftTaskDetailsLabel.for = "timeLeftTaskDetail"
+        timeLeftTaskDetailsLabel.innerText = "Time Left"
+        timeLeftDetailsContainer.appendChild(timeLeftTaskDetailsLabel)
 
         const timeLeftTaskDetails = document.createElement("div")
         timeLeftTaskDetails.classList.add("timeLeftTaskDetail")
+        timeLeftTaskDetails.classList.add("detail")
         const timeDiff = Time.getTimeDifference(task.date)
         if(timeDiff == null) timeLeftTaskDetails.innerText = "Expired"
         else timeLeftTaskDetails.innerText = timeDiff
-        containerTaskDetails.appendChild(timeLeftTaskDetails)
+        timeLeftDetailsContainer.appendChild(timeLeftTaskDetails)
 
-        const priorityTaskDetails = document.createElement("div")
-        priorityTaskDetails.classList.add("priorityTaskDetail")
-        priorityTaskDetails.innerText = task.priority
-        containerTaskDetails.appendChild(priorityTaskDetails)
+        /////
+        const thirdRow = document.createElement("div")
+        thirdRow.classList.add("thirdRow")
+        thirdRow.classList.add("row")
+        thirdRow.appendChild(dateDetailsContainer)
+        thirdRow.appendChild(timeDetailsContainer)
+        thirdRow.appendChild(timeLeftDetailsContainer)
+        containerTaskDetails.appendChild(thirdRow)
+
+        // Fourth Row
+        const notesDetailsContainer = document.createElement("div")
+        notesDetailsContainer.classList.add("detailsContainer")
+
+        const notesTaskDetailsLabel = document.createElement("label")
+        notesTaskDetailsLabel.classList.add("notesTaskDetailsLabel")
+        notesTaskDetailsLabel.classList.add("label")
+        notesTaskDetailsLabel.for = "notesTaskDetail"
+        notesTaskDetailsLabel.innerText = "Notes"
+        notesDetailsContainer.appendChild(notesTaskDetailsLabel)
 
         const notesTaskDetails = document.createElement("div")
         notesTaskDetails.classList.add("notesTaskDetail")
+        notesTaskDetails.classList.add("detail")
         notesTaskDetails.innerText = task.notes
-        containerTaskDetails.appendChild(notesTaskDetails)
+        notesDetailsContainer.appendChild(notesTaskDetails)
+
+        const fourthRow = document.createElement("div")
+        fourthRow.classList.add("fourthRow")
+        fourthRow.classList.add("row")
+        fourthRow.appendChild(notesDetailsContainer)
+        containerTaskDetails.appendChild(fourthRow)
 
         const contentTaskDetails = document.querySelector("#contentTaskDetails")
         contentTaskDetails.appendChild(containerTaskDetails)
