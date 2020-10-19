ea8a6ec3c2979700989b860f3b7a5416fdaf8399
Sasoun Torossian <sasountorossian@gmail.com>
2020-10-19 19:42:41 +0100

    Added commenting to logicController.js.

--- src/Logic/logicController.js
+++ src/Logic/logicController.js
@@ -1,18 +1,23 @@
 import {Factory} from "./factory"
 
+// Responsible for controlling and manipulating project and task objects. Primarily used in domController.js
 const LogicController = (() => {
     let projects = []
     let currentProj = null
     let currentTask = null
 
+    // Pushes new project object into array
     const addProject = (title) => projects.push(Factory.projectFactory(title))
 
+    // Pushes new task object into chosen project's internal array
     const addTask = (indexP, title, desc, date, priority, notes) => projects[indexP].tasks.push(Factory.taskFactory(title, desc, date, priority, notes))
 
+    // Edit project if input was provided
     const editProject = (index, title) => {
         if(title) projects[index].title = title
     }
 
+    // Edit task using provided inputs. Allows variable number of attributes to be edited
     const editTask = (indexP, indexT, title, desc, date, priority, notes) => {
         if(title) projects[indexP].tasks[indexT].title = title
         if(desc) projects[indexP].tasks[indexT].desc = desc
@@ -21,35 +26,49 @@ const LogicController = (() => {
         if(notes) projects[indexP].tasks[indexT].notes = notes
     }
 
+    // Remove project from array
     const removeProject = (index) => projects.splice(index, 1)
 
+    // Remove task from chosen project's internal array
     const removeTask = (indexP, indexT) => projects[indexP].tasks.splice(indexT, 1)
 
+    // Used by Storage.load() to load in saved projects, and set as the new LogicController module projects array
     const setProjects = (projs) => projects = projs 
 
+    // Sets the current selected project based on index
     const setCurrentProject = (index) => currentProj = projects[index]
 
+    // Sets the current selected task based on index
     const setCurrentTask = (index) => currentTask = currentProj.tasks[index]
 
+    // Gets the index of the current selected project
     const getCurrentProjectIndex = () => projects.findIndex(p => p == currentProj)
 
+    // Gets the index of the current selected task
     const getCurrentTaskIndex = () => currentProj.tasks.findIndex(p => p == currentTask)
 
+    // Gets current project
     const getCurrentProject = () => currentProj
 
+    // Gets current task
     const getCurrentTask = () => currentTask
 
+    // Gets index of project immediately after deleted project. modulo in order to wrap around if deleted element was last in the array.
     const getNextProjectIndex = (index) => projects.length != 0 ? index % projects.length : null
 
+    // Gets index of task immediately after deleted task. modulo in order to wrap around if deleted element was last in the array.
     const getNextTaskIndex = (index) => getTasks().length != 0 ? index % getTasks().length : null
 
+    // Get all projects
     const getProjects = () => projects
 
+    // Get all tasks
     const getTasks = () => {
         if(!currentProj) return []
         return  currentProj.tasks
     }
 
+    // If no saved projects exist, load the default projects and tasks for display purposes.
     const addDefault = () => {
         addProject("A")
         addProject("B")
